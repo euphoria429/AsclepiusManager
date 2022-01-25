bc658c2bbfd99835dbca0da5af0569be6a74e420
huangcaixia <1354529884@qq.com>
2022-01-25 14:28:40 +0800

    Initial commit

---
+++ src/main/java/com/hcx/asclepiusmanager/AsclepiusManagerApplication.java
@@ -0,0 +1,14 @@
+package com.hcx.asclepiusmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class AsclepiusManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsclepiusManagerApplication.class, args);
+    }
+
+}
